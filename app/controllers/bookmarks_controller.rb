a1c98ed8250cd69122278a93c1ce69e1f819379c
Paul Henri Eichhorn <paul.henri.eichhorn@gmail.com>
2021-11-12 12:40:40 +0100

    ff

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -27,17 +27,3 @@ class BookmarksController < ApplicationController
     params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
-
-# BookmarksController
-#   GET new
-#     assigns a new bookmark to @list (FAILED - 1)
-#   POST create
-#     with valid params
-#       creates a new bookmark (FAILED - 2)
-#       assigns a newly created bookmark as @bookmark (FAILED - 3)
-#       redirects to the created list (FAILED - 4)
-#     with invalid params
-#       assigns a newly created but unsaved bookmark as @bookmark (FAILED - 5)
-#       re-renders the 'new' template or 'lists/show' (FAILED - 6)
-#   DELETE destroy
-#     deletes a bookmark (FAILED - 7)
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -26,17 +26,3 @@ class ListsController < ApplicationController
     params.require(:list).permit(:name)
   end
 end
-
-
-  # GET show
-  #   assigns the requested list as @list (FAILED - 6)
-  # GET new
-  #   assigns a new list as @list (FAILED - 7)
-  # POST create
-  #   with valid params
-  #     creates a new List (FAILED - 8)
-  #     assigns a newly created list as @list (FAILED - 9)
-  #     redirects to the created list (FAILED - 10)
-  #   with invalid params
-  #     assigns a newly created but unsaved list as @list (FAILED - 11)
-  #     re-renders the 'new' template (FAILED - 12)
